b1b0dd3ea15bf6c5d9721698214b672e2f22f118
Paul Pasaca <paul.pasaca@unl.edu.ec>
2018-02-07 15:06:50 -0500

    Add files via upload

--- src/datos/Banco.java
+++ src/datos/Banco.java
@@ -3,50 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package datos;
+package examen2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author Jorge Pucha
+ * @author PAUL PASACA
  */
 public class Banco {
-
     static ArrayList<Cuenta> listaCuenta = new ArrayList<Cuenta>();
-    static String archivo = "";
+    static String archivo = "asd.csv";
 
     public static void agregar(Cuenta cuenta) {
         listaCuenta.add(cuenta);
     }
     
-    public static void grabar() {
-        PrintWriter pw = null;
-        try {
-	    // Examen002: La ruta y el nombre del 'archivo' debe ser 
-	    // establecido dinamicamente por el usuario en el lugar adecuado
-            FileWriter fw = new FileWriter(archivo, true);
-            pw = new PrintWriter(fw);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        for (Cuenta cuenta : listaCuenta) {
-            String linea = "";
-            if (cuenta instanceof CuentaAhorro) {
-                linea="Cuenta Ahorro"+";"+cuenta.toString();
-            }if (cuenta instanceof CuentaHipoteca) {
-                linea="Cuenta Hipoteca"+";"+cuenta.toString();
-            }if (cuenta instanceof CuentaPrestamo) {
-                linea="Cuenta Prestamo"+";"+cuenta.toString();
-            }
-            pw.println(linea);
-        }
-        pw.close();
-    }
-    
+
     public static Cuenta buscarCuentaAhorro() {
         String nombre = (JOptionPane.showInputDialog(null, "Ingrese nombre", "Verificar Cuenta", JOptionPane.INFORMATION_MESSAGE));
         CuentaAhorro cuentaAH = new CuentaAhorro(nombre);
@@ -56,7 +36,7 @@ public class Banco {
         }
         return null;
     }
-    public static Cuenta buscarCuentaHipoteca() {
+    public  Cuenta buscarCuentaHipoteca() {
         String nombre = (JOptionPane.showInputDialog(null, "Ingrese nombre", "Verificar Cuenta", JOptionPane.INFORMATION_MESSAGE));
         CuentaHipoteca cuentaAH = new CuentaHipoteca(nombre);
         Cuenta c=(Cuenta)cuentaAH;
@@ -65,13 +45,13 @@ public class Banco {
         }
         return null;
     }
-
-    public static void leerCuentas(){
-	/* 
-	 * Lee los datos desde un archivo de texto, crea objetos 'Cuenta'
-	 * y los almacena en la lista 'listaCuenta'
-	 */
-	// Examen 002: Completar este metodo
-
+    public  void presentar(){
+        String linea = "";
+        for (Cuenta cuenta : listaCuenta) {
+            linea = linea +cuenta.toString()+"\n";
+        }
+        JOptionPane.showMessageDialog(null, linea);
     }
+
+   
 }
--- src/datos/Cuenta.java
+++ src/datos/Cuenta.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package datos;
+package examen2;
 
 import java.util.Objects;
 
 /**
  *
- * @author Jorge Pucha
+ * @author PAUL PASACA
  */
 public abstract class Cuenta {
     private String cliente;
